38d19e4edf4d0d361d34606cb7e36d9f9fbda2b1
Gözde Budak <gzbudak@gmail.com>
2023-03-27 20:21:05 +0300

    Added User class, its service and controller

--- src/main/java/com/gozdebudak/rest/webservices/socialmediarestapi/user/UserDaoService.java
+++ src/main/java/com/gozdebudak/rest/webservices/socialmediarestapi/user/UserDaoService.java
@@ -1,13 +1,22 @@
 package com.gozdebudak.rest.webservices.socialmediarestapi.user;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.springframework.stereotype.Component;
 
 //Data Access Object
 @Component
 public class UserDaoService {
-    private static List<User> users;
+    private static List<User> users = new ArrayList<>();
+
+    static{
+        users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(2, "Eve", LocalDate.now().minusYears(25)));
+        users.add(new User(3, "Jim", LocalDate.now().minusYears(20)));
+    }
 
     public List<User> findAllUsers(){
         return users;
@@ -18,6 +27,7 @@ public class UserDaoService {
     }
 
     public User findUser(Integer id){
-        return null;
+        Predicate<? super User> predicate = user -> user.getId().equals(id); 
+        return users.stream().filter(predicate).findFirst().get();
     }
 }
