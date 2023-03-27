c79b78c048e890ab6124197616d59daf5b8a948a
Gözde Budak <gzbudak@gmail.com>
2023-03-27 20:49:15 +0300

    Added retrieveUser and createUser methods

--- src/main/java/com/gozdebudak/rest/webservices/socialmediarestapi/user/UserResource.java
+++ src/main/java/com/gozdebudak/rest/webservices/socialmediarestapi/user/UserResource.java
@@ -3,6 +3,9 @@ package com.gozdebudak.rest.webservices.socialmediarestapi.user;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +20,15 @@ public class UserResource {
     @GetMapping(path = "/users")
     public List<User> retrieveAllUsers(){
         return service.findAllUsers();
+    }
+    
+    @GetMapping(path = "/users/{id}")
+    public User retrieveUser(@PathVariable Integer id){
+        return service.findUser(id);
+    }
+
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user){
+        service.save(user);
     }
 }
