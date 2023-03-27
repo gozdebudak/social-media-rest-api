3faa107d7372010acbfc8c5a1abc1d8cb4e06f9a
Gözde Budak <gzbudak@gmail.com>
2023-03-27 19:49:13 +0300

    Inital commit

---
+++ src/test/java/com/gozdebudak/rest/webservices/socialmediarestapi/SocialMediaRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gozdebudak.rest.webservices.socialmediarestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
