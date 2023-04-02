79a88ff9e6723722252a3022b0b99d0bac80fd50
Gözde Budak <gzbudak@gmail.com>
2023-04-02 16:03:44 +0300

    Added location url for create user post request

--- src/main/java/com/gozdebudak/rest/webservices/socialmediarestapi/user/UserResource.java
+++ src/main/java/com/gozdebudak/rest/webservices/socialmediarestapi/user/UserResource.java
@@ -1,5 +1,6 @@
 package com.gozdebudak.rest.webservices.socialmediarestapi.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -30,7 +32,9 @@ public class UserResource {
 
     @PostMapping("/users")
     public ResponseEntity<User> createUser(@RequestBody User user){
-        service.save(user);
-        return ResponseEntity.created(null).build();
+        User createdUser = service.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                        .path("/{id}").buildAndExpand(createdUser.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 }
